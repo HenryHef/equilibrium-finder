7e7ea257c283bc3d75bf2a05b80b6a272aebec6b
HenryHef <henryhef12345@gmai.com>
2016-12-04 21:49:10 -0500

    edited some small changes

--- EquilibriumFinder.py
+++ EquilibriumFinder.py
@@ -35,7 +35,7 @@ def Q(equilibriumEquation,molarityMap,x=0):#if dividing by zero, return Q=1E200,
     #print "(bottom,top,x,molarityMap,Q)="+str((bottom,top,x,molarityMap,Q))
     return Q
 
-def getShiftToEq(e,M,xMin,xMax):
+def getShiftToEq(e,M,xMin,xMax,maxSerchRounds=1000):
     products,reactants,Keq=e
     #since Q monitonicly increasing or decreasing on xMin to xMax
     xSmall=xMin*.75+xMax*.25
@@ -57,7 +57,7 @@ def getShiftToEq(e,M,xMin,xMax):
         return xMax
     xMinL=1E250
     xMaxL=-1E250
-    for i in range(0,300): #change to distance from Keq?
+    for i in range(0,maxSerchRounds): #change to distance from Keq?
         xTest=(xMin+xMax)/2.0
         Qt=Q(e,M,xTest)
         if(xMin==xMinL and xMax==xMaxL):
@@ -73,13 +73,13 @@ def getShiftToEq(e,M,xMin,xMax):
             xMin=xTest
             Qmin=Qt
         else:
-            print "out of range error"
+            print "error"
         #print "step LS= "+str(i)+"(xMin,xMax)= "+str((xMin,xMax,(xMin+xMax)/2.0))
     return (xMin+xMax)/2.0
 
 
 
-def shiftMap(equilibriumEquation,molarityMap,N):
+def shiftMap(equilibriumEquation,molarityMap,N,maxSerchRounds=1000):
     #print equilibriumEquation
     reactants,products,Keq=equilibriumEquation
     xMinL=[-molarityMap[prod[0]]*1.0/prod[1] for prod in products]
@@ -90,7 +90,7 @@ def shiftMap(equilibriumEquation,molarityMap,N):
 
     #print "X-RANGE="+str((xMin,xMax))
 
-    X=getShiftToEq(equilibriumEquation,molarityMap,xMin,xMax)
+    X=getShiftToEq(equilibriumEquation,molarityMap,xMin,xMax,maxSerchRounds)
 
     shift=X*N
 
@@ -109,20 +109,19 @@ def checkEQ(equilibriumEquationList,molarityMap):
         if r<1:
             r=1.0/r
         rMax=max(rMax,r)
-    print "max error = "+str((rMax-1.0)*100.0)+"%"
+    print "max error Q from Keq = "+str((rMax-1.0)*100.0)+"%"
 
-def findEquilibriumFinal(equilibriumEquationList,initialMolarityMap):
+def findEquilibriumFinal(equilibriumEquationList,initialMolarityMap,maxApplyRounds=1000,maxSerchRounds=1000,debug=False):
     molarityMap = {}
     for key in initialMolarityMap:
         molarityMap[key]=initialMolarityMap[key]
     
     oldMolarityMap = {}
     N=.5
-    rounds=1000
     for key in initialMolarityMap:
         oldMolarityMap[key]=0
-    for i in range(0,rounds):
-        print "ROUND: "+str(i)+"\t"+str(molarityMap)
+    for i in range(0,maxApplyRounds):
+        if(debug):print "ROUND: "+str(i)+"\t"+str(molarityMap)
         #first check no change
         if all(oldMolarityMap[key]==molarityMap[key] for key in initialMolarityMap):
             break
@@ -133,16 +132,14 @@ def findEquilibriumFinal(equilibriumEquationList,initialMolarityMap):
             oldMolarityMap[key]=molarityMap[key]
         #update rConstants
         for equation in equilibriumEquationList: #in the form ((reactants),(products),Keq)
-            shiftMap(equation,molarityMap,N)
+            shiftMap(equation,molarityMap,N,maxSerchRounds)
     return molarityMap
 
 def makeEquation(reactants,products,Keq):
     return (reactants,products,Keq)
 
 def fillInitListWithMissing0(m,Es):
-    print "filling"
     for e in Es:
-        print "e="+str(e)
         for reac in e[0]:
             if not reac[0] in initialMolarityMap:
                 initialMolarityMap[reac[0]]=0
@@ -157,10 +154,6 @@ def anyOverlap(lA,lB):
     return False
 
 def makeCombEq(a,b,fliped):
-    print "combing"
-    print "a="+str(a)
-    print "b="+str(b)
-    print "fliped="+str(fliped)
     r1=a[0]
     p1=a[1]
     r2=b[1] if fliped else b[0]
@@ -177,14 +170,12 @@ def makeCombEq(a,b,fliped):
                 r.remove(reac)
                 p.remove(reac)
     res=makeEquation(r,p,a[2]*(1.0/b[2] if fliped else b[2]))
-    print "res="+str(res)+"\n"
     return res
 
 
 def genCombEqsN_is_2(es):
     re=[]
     for a,b in itertools.combinations(es, 2):
-        print "a,b = "+str((a,b))
         if anyOverlap(a[0],b[0]) or anyOverlap(a[1],b[1]):
             re.append(makeCombEq(a,b,True))
         if anyOverlap(a[0],b[1]) or anyOverlap(a[0],b[1]):
@@ -218,28 +209,25 @@ def removeDoubles(eqs):
 
 def genCombEqs(es):
     last=genCombEqsN_is_2(es)
-    print "last = "+str(last)
     re=last
     for n in range(len(es)):
         last=genCombEqsN_is_more_than_2(es,last)
-        print "last = "+str(last)
         re.extend(last)
-        print "re = "+str(re)
 
     re = removeDoubles(re)
-    print "re = "+str(re)
     return re
 
-def findEquilibrium(equilibriumEquationList,initialMolarityMap):
+def findEquilibrium(equilibriumEquationList,initialMolarityMap,maxApplyRounds=1000,maxSerchRounds=1000,debug=False):
     fillInitListWithMissing0(initialMolarityMap,equilibriumEquationList)
     extraEquations=genCombEqs(equilibriumEquationList)
-    for eq in extraEquations:
-        equilibriumEquationList.append(eq)
-    mMap=findEquilibriumFinal(equilibriumEquationList,initialMolarityMap)
+    equilibriumEquationList.extend(extraEquations)
+    mMap=findEquilibriumFinal(equilibriumEquationList,initialMolarityMap,maxApplyRounds,maxSerchRounds,debug)
     print "\n\n"
     print mMap
     print "ph="+str(-np.log10(mMap["H+"]))
     checkEQ(equilibriumEquationList,mMap)
+
+#TODO add text equation parser
 
 
 e1=makeEquation([],[("H+",1),("OH-",1)],1E-14)
